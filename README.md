1f608a32c96302b474054773471d0866e156ed80
inemyrovsk <ivan.nemyrovskiy@gmail.com>
2022-10-29 16:03:59 +0300

    app

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# helm_demo
+# helm_demo3
+
+#### variables:
+```
+HOST=0.0.0.0
+PORT=8000
+DEPLOY_ENV=test
+```
+### contribute:
+#### 1. setup virtual environment for python
+
+
+```
+pip install virtualenv && \
+python3 -m venv demo && \
+source demo/bin/activate
+```
+
+#### 2. install dependencies
+```
+pip install -r ./app/requirements.txt
+```
+
+### 3 spin up application locally
+
+#### 3.1 build docker image
+```
+docker build -t demo
+```
+#### 3.2 start docker container
+```
+docker run -p 8000:8000 -d --name demo_app demo
+```
+
+## Stop project 
+### 1. stoip app
+```
+docker rm --force demo_app
+```
+### 2. deactivate virtual environment
+```
+deactivate
+```
---
+++ app/wsgi.py
@@ -0,0 +1,34 @@
+import os
+import socket
+from flask import Flask, render_template
+
+ENVIRONMENT = "TEST"
+# ENVIRONMENT = os.getenv('DEPLOY_ENV')
+HOST = "0.0.0.0"
+# HOST = os.getenv('HOST')
+PORT = 8000
+# PORT = os.getenv('PORT')
+
+app = Flask(ENVIRONMENT)
+
+
+@app.route("/")
+def hello():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    print(f"Hostname: {hostname}")
+    print(f"IP Address: {ip_address}")
+    with open('/mnt/.secret', 'r') as secret:
+        secret_str, secret_str1, secret_str2 = [x for x in secret.readlines()]
+    with open('/mnt/.super-secret', 'r') as super_secret:
+        super_secret_str, super_secret_str1 = [x for x in super_secret.readlines()]
+    return render_template(
+        'index.html', ip_addr=ip_address, hostname=hostname, secret_string=secret_str,
+        secret_str1=secret_str1, secret_str2=secret_str2,
+        super_secret_str=super_secret_str,
+        super_secret_str1=super_secret_str1
+    )
+
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT)
